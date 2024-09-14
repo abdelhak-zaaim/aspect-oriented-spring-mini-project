42b1f9f0d04b39f972ecf6a134d13e60adbfdcd7
Abdelhak Zaaim <abdelhakzammii@gmail.com>
2024-09-14 14:09:25 +0100

    Initial commit

---
+++ src/main/java/com/zaaim/aop/AspectOrientedSpringMiniProjectApplication.java
@@ -0,0 +1,13 @@
+package com.zaaim.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AspectOrientedSpringMiniProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AspectOrientedSpringMiniProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zaaim/aop/AspectOrientedSpringMiniProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaaim.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AspectOrientedSpringMiniProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
